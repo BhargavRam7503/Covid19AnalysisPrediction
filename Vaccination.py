785c5337f1736ddd1cf98f2ec22b34d04d47600a
BhargavRam7503 <52447316+BhargavRam7503@users.noreply.github.com>
2021-06-07 14:56:36 +0530

    Update Vaccination.py

--- Vaccination.py
+++ Vaccination.py
@@ -1,6 +1,7 @@
 import json
 import folium
 import plotly
+import geopandas as gpd
 import plotly.express as px
 import plotly.figure_factory as ff
 import plotly.graph_objects as go
@@ -14,9 +15,12 @@ class vaccination:
                                         country = i
                                         break
                 folium.GeoJson(country,name='India').add_to(map)
+                map_child = folium.features.GeoJson(vaccine_mapmaker_data, tooltip=folium.features.GeoJsonTooltip(
+                        fields=['State','Total Individuals Vaccinated','Total Estimated Population','Vaccine Percent'],
+                        style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;") ) )
+                map.add_child(map_child)
+                map.keep_in_front(map_child)
                 folium.LayerControl().add_to(map)
-                for lat, lon, value, name in zip(vaccine_mapmaker_data['Latitude'],vaccine_mapmaker_data['Longitude'], vaccine_mapmaker_data['Total Individuals Vaccinated'], vaccine_mapmaker_data['State']):
-                        folium.CircleMarker([lat, lon], radius=value*0.000002, popup = ('<strong>State</strong>: ' + str(name).capitalize() + '<br>''<strong>Total Individuals Vaccinated</strong>: ' + str(value) + '<br>'),color='Green',fill_color="Green",fill_opacity=0.3 ).add_to(map)
                 return map
         def vfigs(df,X,Y,title):
                 return px.bar(df,x=X,y=Y,color_discrete_sequence=px.colors.qualitative.Dark2,title='<b>'+title+'</b>')
